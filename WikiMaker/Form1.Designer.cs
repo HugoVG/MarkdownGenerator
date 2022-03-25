67d4462db9cd60be1f00f60a9654daf3d4a4fe17
HugoVG <hugovangeijn@gmail.com>
2022-03-25 10:56:36 +0100

    updated for 2022
    Ported to dotnet6.0
    ported to WinForm for more accessibility

---
+++ WikiMaker/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WikiMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.Worked = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(106, 39);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(112, 34);
+            this.SelectFile.TabIndex = 0;
+            this.SelectFile.Text = "Select DLL";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Worked
+            // 
+            this.Worked.AutoSize = true;
+            this.Worked.Location = new System.Drawing.Point(155, 136);
+            this.Worked.Name = "Worked";
+            this.Worked.Size = new System.Drawing.Size(0, 25);
+            this.Worked.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 500);
+            this.Controls.Add(this.Worked);
+            this.Controls.Add(this.SelectFile);
+            this.Name = "Form1";
+            this.Text = "WikiMaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button SelectFile;
+        private Label Worked;
+    }
+}
---
+++ WikiMaker/Form1.cs
@@ -0,0 +1,80 @@
+using MarkdownWikiGeneratorCore;
+using System.Diagnostics;
+using System.Text;
+
+namespace WikiMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string target = string.Empty;
+            string dest = "docs";
+            string namespaceMatch = string.Empty;
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                target = openFileDialog1.FileName;
+                Debug.WriteLine(target);
+            }
+            MarkdownableType[] types = MarkdownGenerator.Load(target, namespaceMatch);
+            Debug.WriteLine("28");
+            // Home Markdown Builder
+            MarkdownBuilder homeBuilder = new MarkdownBuilder();
+            homeBuilder.Header(1, "References");
+            homeBuilder.AppendLine();
+            Debug.WriteLine("33");
+            bool working = false;
+            foreach (IGrouping<string, MarkdownableType> g in types.GroupBy(x => x.Namespace).OrderBy(x => x.Key))
+            {
+                working = true;
+                if (Directory.Exists(dest))
+                {
+                    Directory.Delete(dest, true);
+                }
+                Directory.CreateDirectory(dest);
+
+                Debug.WriteLine("40");
+                homeBuilder.HeaderWithLink(2, g.Key, g.Key);
+                homeBuilder.AppendLine();
+                Debug.WriteLine("43");
+                StringBuilder sb = new StringBuilder();
+                foreach (MarkdownableType item in g.OrderBy(x => x.Name))
+                {
+                    homeBuilder.ListLink(MarkdownBuilder.MarkdownCodeQuote(item.BeautifyName), g.Key + "#" + item.BeautifyName.Replace("<", "").Replace(">", "").Replace(",", "").Replace(" ", "-").ToLower());
+                    Debug.WriteLine("48");
+                    sb.Append(item.ToString());
+                }
+                Debug.WriteLine("51");
+                File.WriteAllText(Path.Combine(dest, g.Key + ".md"), sb.ToString());
+                homeBuilder.AppendLine();
+            }
+            if(working == false)
+            {
+                Worked.Text = "Unsupported project/dll or not selected as Debug.dll";
+            }
+            else
+            {
+                Process.Start("explorer.exe", Path.Combine(Environment.CurrentDirectory, "docs"));
+            }
+            
+            Debug.WriteLine("55");
+            // Gen Home
+            File.WriteAllText(Path.Combine(dest, "Home.md"), homeBuilder.ToString());
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); Debug.WriteLine("59");// <-- For debugging use. 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
